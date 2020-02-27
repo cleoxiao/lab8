0b82eaaf6b18bcd7f83ac4a019788e24f321b047
cleoxiao <yax064@ucsd.edu>
2020-02-27 12:05:57 -0800

    done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '5eV7vYp92bHwUsa969hAcLGrWefUQbbM';
+
+	var map = L.mapquest.map('map',{
+	  center:[32.88, -117.236],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom:12,
+	  zoomControl: false
+	});
+  
+	L.marker([32.88, -117.236]).addTo(map);
 }
